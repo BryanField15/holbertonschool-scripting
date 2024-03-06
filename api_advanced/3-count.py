ee4c6cb4cc7b96aa69c64987ee9dddaaaebcb371
BryanField15 <4bryanfield@gmail.com>
2024-03-06 18:40:19 +1100

    Comment

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module queries the Reddit API, parses the title of all hot articles, 
+and prints a sorted count of given keywords (case-insensitive, 
+delimited by spaces
+"""
 
 import requests
 
